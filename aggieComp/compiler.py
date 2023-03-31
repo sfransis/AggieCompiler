3da053b7663645d311d6a60f6ad440934f822f01
Kevin Haro <kharo019@kharo019ca6f.nmsu.edu>
2023-03-31 13:14:36 -0600

    Committer: Kevin Haro <kharo019@kharo019ca6f.nmsu.edu>
     On branch main
     Your branch is behind 'origin/main' by 3 commits, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   ../.DS_Store#
    	modified:   __pycache__/dbModels.cpython-310.pyc
    	modified:   compiler.py
    	modified:   instance/database.db
    
     Untracked files:
    	__pycache__/compiler.cpython-310.pyc
    	__pycache__/compiler.cpython-311.pyc
    	__pycache__/dbModels.cpython-311.pyc
    	__pycache__/qANDa.cpython-310.pyc
    	__pycache__/qANDa.cpython-311.pyc
    	__pycache__/roadmap.cpython-310.pyc
    	__pycache__/roadmap.cpython-311.pyc
    	__pycache__/run.cpython-310.pyc
    	__pycache__/run.cpython-311.pyc

--- aggieComp/compiler.py
+++ aggieComp/compiler.py
@@ -1,7 +1,28 @@
 from flask import Blueprint, Flask, redirect, url_for, render_template, request, session, flash
 
+# My importation libraries 
+from PyQt6 import * 
+from PyQt6.QtWidgets import * 
+from PyQt6.QtCore import * 
+from PyQt6.QtGui import * 
+from PyQt6.Qsci import * 
+
+from pathlib import Path
+
+import sys 
+import os
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+
+
+
+
 compiler = Blueprint("compiler", __name__, static_folder = "static", template_folder = "template")
 
+# this will create the route of the url 
 @compiler.route("/compiler")
 def compilerPage():
+    
     return render_template("compiler.html")
